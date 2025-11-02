bbad906f2d29b383e12a84aacbc4b9dc7d04ba86
kansei-matsumoto <matsukan1012@gmail.com>
2025-11-03 02:13:28 +0900

    画面の大まかな枠組みと画面にかかわるcss,tsを作成しました。

---
+++ src/scripts/main.ts
@@ -0,0 +1,44 @@
+import { invoke } from "@tauri-apps/api/core";
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  
+  // サイドバーの制御
+  const sidebarContents = document.getElementById("sidebar-contents") as HTMLElement;
+  const sidebarToggle = document.getElementById("sidebar-toggle") as HTMLElement;
+
+  sidebarToggle.addEventListener("click", () => {
+    sidebarContents.classList.toggle("active");
+    if(sidebarContents.classList.contains("active")){
+      sidebarToggle.textContent="×";
+    }else{
+      sidebarToggle.textContent="|||";
+    }
+  });
+
+  // モーダル画面の制御
+  document.querySelectorAll(".modal-button").forEach(modalButton=>{
+    const dialog = document.getElementById("modal-"+modalButton.id) as HTMLDialogElement;
+    modalButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+    document.getElementById("close-"+modalButton.id)?.addEventListener("click",() => {
+      dialog.close();
+    });
+  });
+
+  // Excelのインポート
+  document.querySelector("#import-button")?.addEventListener("click",()=>{
+    importExcel();
+  });
+});
+
+async function importExcel() {
+  const result = await invoke("importExcel", {
+    path: "../../template1.xlsx"
+  });
+  console.log(result);
+}
+
+
